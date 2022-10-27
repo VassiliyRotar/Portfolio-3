d8cc650c11ffe6c2e22a2cd3f142a8b369f79d38
VassiliyRotar <vassiliyrotar@gmail.com>
2022-10-27 15:37:11 +0600

    add Portfolio-3

---
+++ js/mobilenav.js
@@ -0,0 +1,9 @@
+const burger = document.getElementById('sidebarToggle');
+const sidebar = document.getElementById('sidebar');
+
+
+burger.addEventListener('click', event => {
+    document.body.classList.toggle('show-sidebar');
+
+});
+
